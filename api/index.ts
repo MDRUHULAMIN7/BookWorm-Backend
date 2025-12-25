ff070d680ae867731a23f1063af6382a85be349d
mdruhulamin7 <ruhulthisis@gmail.com>
2025-12-25 19:21:49 +0600

    add  root api file

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import app from '../src/app.js';
+import config from '../src/app/config/index.js';
+import mongoose from 'mongoose';
+
+// Connect to MongoDB
+mongoose.connect(config.database_url as string)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Export the Express app for Vercel serverless
+export default app;
